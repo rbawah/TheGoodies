09b6984254096391c9bb186a2ce1da1974ae4b75
Rashid Bawah <arbawah1@gmail.com>
2021-12-18 11:46:50 -0700

    Reset

--- example-2-7.py
+++
@@ -1,8 +0,0 @@
-# Tuples used as records
-
-traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
-for passport in sorted(traveler_ids):
-    print('%s/%s' % passport)
-print('======')
-for passport in traveler_ids:
-    print('%s --> %s' % passport)
