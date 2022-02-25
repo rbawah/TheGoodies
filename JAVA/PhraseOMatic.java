7b02e16e1ec388da92fa272227753e8bef26b655
Rashid Bawah <arbawah1@gmail.com>
2022-02-25 15:02:26 -0700

    Algos

---
+++ JAVA/PhraseOMatic.java
@@ -0,0 +1,7 @@
+public class PhraseOMatic {
+   public static void main (String[] args) {
+    String[] pets = {"Fido", "Zeus", "Bin"};
+    int x = pets.length;
+    System.out.print(pets);
+   }
+}
