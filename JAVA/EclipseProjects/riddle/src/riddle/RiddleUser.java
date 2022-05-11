04225d1fdb6bf9437f5a89ce62f0046dc74d0320
Rashid Bawah <arbawah1@gmail.com>
2022-05-11 12:58:02 -0600

    Added RiddleUser.java

---
+++ JAVA/EclipseProjects/riddle/src/riddle/RiddleUser.java
@@ -0,0 +1,10 @@
+//package riddle;
+
+public class RiddleUser {
+
+	public static void main(String[] args) {
+		
+		Riddle riddle = new Riddle("What is black and white and red all over?", "An embarrassed Zebra.");
+	}
+
+}
