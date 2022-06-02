e3259607c44d6c345f8e87d612cfb8cbb2074b18
Rashid Bawah <arbawah1@gmail.com>
2022-06-02 17:02:22 -0600

    added test

--- algorithms++/peakfinding_naive.py
+++ algorithms++/peakfinding_naive.py
@@ -1,12 +1,15 @@
 
 # find a peak in 1D array - naive (linear search) approach
-def find_peak(arr, peak):
+def find_peak(arr):
     peak = 0
     if len(arr) == 0:
         return -1
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         if arr[i] >= peak:
             peak = arr[i]
-            return peak
-    return -1
+    return peak
+   
 
+arr = [ 1, 3, 20, 4, 1, 0 ]
+# n = len(arr)
+print("The peak point is", find_peak(arr))
